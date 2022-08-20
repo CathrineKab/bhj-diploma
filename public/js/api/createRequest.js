b1474d91bf957170ad9624d2e7ff9ee987ce3bc6
CathrineKab <99976784+CathrineKab@users.noreply.github.com>
2022-08-20 17:06:03 +0300

    Update createRequest.js

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -1,4 +1,9 @@
-const createRequest = (options = {}) => {
+/**
+ * Основная функция для совершения запросов
+ * на сервер.
+ * */
+
+ const createRequest = (options = {}) => {
 	const xhr = new XMLHttpRequest();
 	const formData = new FormData();
 
@@ -33,59 +38,3 @@ const createRequest = (options = {}) => {
 		} else err = new Error('Что-то пошло не так...');
 	} 
 }
-
-    return {
-        queryString: '',
-        formData: buildFormData(data),
-    }
-};
-
-const isCorrectCallback = (callback) => callback && typeof callback === 'function';
-
-/**
- * Основная функция для совершения запросов
- * на сервер.
- * */
-const createRequest = (options = {}) => {
-    const { url, method, data, callback, responseType } = option;
-
-    if (!option.url) {
-        throw new Error('Не указан url запроса');
-    }
-
-    if (!option.method) {
-        throw new Error('Не указан метод запроса');
-    }
-
-    const { queryString, formData } = getRequestDataByMethod(method, data);
-
-    const reuestUrl = `${url}${queryString.length > 0 ? '?' + queryString : ''}`;
-
-    const xhr = new XMLHttpRequest();
-    xhr.responseType = responseType ?? JSON_RESPONSE_TYPE;
-
-    xhr.onload = function () {
-        if (!isCorrectCallback(callback)) {
-            return;
-        }
-
-        try {
-            if (xhr.status === 200) {
-                callback(null, xhr.response);
-            } else {
-                callback(new Error(`${xhr.status}: ${xhr.statusText}`));
-            }
-        } catch (error) {
-            callback(new Error(error));
-        }
-    };
-
-    xhr.open(method, reuestUrl);
-
-    if (isGetMethod(method)) {
-        xhr.send();
-        return;
-    }
-
-    xhr.send(formData);
-};
