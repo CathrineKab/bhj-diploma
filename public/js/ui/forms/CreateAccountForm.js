05921c58ecf3e3b78b70c6bad9a732ace2842fda
CathrineKab <99976784+CathrineKab@users.noreply.github.com>
2022-08-21 20:47:16 +0300

    Update CreateAccountForm.js

--- public/js/ui/forms/CreateAccountForm.js
+++ public/js/ui/forms/CreateAccountForm.js
@@ -9,7 +9,7 @@ class CreateAccountForm extends AsyncForm {
    * и сбрасывает форму
    * */
   onSubmit(data) {
-Account.create(data, (err, response) => {
+    Account.create(data, (err, response) => {
       if (response && response.success) {
         App.update();
         App.getModal('createAccount').close();
